f105b07eb31783836fafaa6337b3567e902febeb
VanVDC <vanbvy@gmail.com>
2020-03-15 21:25:38 -0400

    added heroku server

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://morning-dusk-84053.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-type": "application/json" },
       body: JSON.stringify({
@@ -89,7 +89,7 @@ class App extends React.Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://morning-dusk-84053.herokuapp.com/image", {
             method: "put",
             headers: { "Content-type": "application/json" },
             body: JSON.stringify({
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -21,7 +21,7 @@ class Register extends React.Component {
   };
 
   onSubmitRegister = () => {
-    fetch("http://localhost:3000/register", {
+    fetch("https://morning-dusk-84053.herokuapp.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
